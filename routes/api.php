2bdcba7bdf66f8ec57ce39705e84698dc626756a
kenny <doikotinh99@gmail.com>
2022-10-11 00:19:30 +0700

    fix api restaurant

--- routes/api.php
+++ routes/api.php
@@ -62,7 +62,7 @@ Route::apiResource("blog", BlogController::class, ["only" => [
 Route::post("/blog/cmt", [BlogController::class, "cmt"])->middleware("auth:sanctum");
 Route::post("/blog/repcmt", [BlogController::class, "replyComment"])->middleware("auth:sanctum");
 Route::post("blog/images", [BlogController::class, "pushImages"]);
-
+Route::apiResource("/restaurant", RestaurantController::class, ["only" => ["index"]]);
 Route::apiResource("/restaurant", RestaurantController::class)->middleware("auth:sanctum");
 Route::post("/restaurant/update", [RestaurantController::class, "update"])->middleware("auth:sanctum");
 
