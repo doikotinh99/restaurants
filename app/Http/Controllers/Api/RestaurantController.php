9428fed9c71cf0b407fa9dec2b220902168db758
doikotinh99 <56866968+doikotinh99@users.noreply.github.com>
2022-11-01 02:01:30 +0700

    01/11/2022

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -101,6 +101,7 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
+        return $request->all();
         $restaurant = Restaurant::create([
             "user_id" => $request->user()->id,
             "name" => $request->name,
