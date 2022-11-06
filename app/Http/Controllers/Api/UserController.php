befd266beb4cf0ef25ba6f0e36fcfcff272db828
doikotinh99 <56866968+doikotinh99@users.noreply.github.com>
2022-11-07 01:36:25 +0700

    test search

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -45,8 +45,7 @@ class UserController extends Controller
     public function index()
     {
         $user = Auth::user();
-        foreach($user as $val)
-            $user->infor;
+        $user->infor;
         return $user;
     }
 
