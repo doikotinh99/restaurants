fb7dbc12b1cc99da085c1cb9b23f46fe1adc8401
doikotinh99 <56866968+doikotinh99@users.noreply.github.com>
2022-11-08 03:33:49 +0700

    user info

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -130,7 +130,7 @@ class UserController extends Controller
      */
     public function update(Request $request, $action)
     {
-        // return $request->all();
+        return $request->all();
         //
         $user = $request->user();
         $check = $this->checkInfor($user->id);
